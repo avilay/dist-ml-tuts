a74f92867674876e676e0ce2c23a5dddd7a62e35
avilay <avilay@ip-172-31-59-147.us-west-2.compute.internal>
2022-08-29 05:14:57 +0000

    prepare done

--- datastuff/criteo/prepare.py
+++ datastuff/criteo/prepare.py
@@ -1,39 +1,62 @@
+import os
+import shutil
+import sys
 from pathlib import Path
+from typing import Optional
 from urllib.parse import urlparse
 
 import boto3
 import click
 import pyarrow as pa
+import pyarrow.csv as pcsv
+import pyarrow.dataset as ds
 import requests
 from botocore.exceptions import ClientError
-from cprint import danger_print, info_print, warning_print
+from cprint import danger_print, warning_print
+from pyarrow.lib import ArrowInvalid
 from tqdm import tqdm
+from glob import glob
 
-DATAROOT = Path.home() / "temp/criteo-kaggle-small/data"
 CHUNK_SZ_BYTES = 1024
 
 
-def download(download_url: str) -> Path:
-    filename = Path(urlparse(download_url).path).stem
-    dst = DATAROOT / "tsv" / filename
-    if dst.exists():
+def print_now(*args, **kwargs):
+    print(*args, **kwargs)
+    sys.stdout.flush()
+
+
+def download(dataroot, download_url: str) -> list[Path]:
+    print()
+    filename = Path(urlparse(download_url).path).name
+    tsvroot = dataroot / "tsv"
+    os.makedirs(tsvroot, exist_ok=True)
+    dst = tsvroot / filename
+
+    if not dst.exists():
+        r = requests.get(download_url, stream=True)
+        total_bytes = int(r.headers.get("content-length", 0))
+        progress_bar = tqdm(total=total_bytes, unit="iB", unit_scale=True)
+        with open(dst, "wb") as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                progress_bar.update(len(chunk))
+                f.write(chunk)
+        progress_bar.close()
+    else:
         warning_print(f"{dst} already exists, skipping downloading.")
-        return dst
 
-    r = requests.get(download_url, stream=True)
-    total_bytes = int(r.headers.get("content-length", 0))
-    progress_bar = tqdm(total=total_bytes, unit="iB", unit_scale=True)
-    with open(dst, "wb") as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            progress_bar.update(len(chunk))
-            f.write(chunk)
+    print_now("Extracting...", end="")
+    shutil.unpack_archive(filename=dst, extract_dir=tsvroot)
+    print_now("Complete.")
 
-    progress_bar.close()
+    # print_now("Deleting archive...", end="")
+    # os.remove(dst)
+    # print_now("Complete.")
 
-    return dst
+    return [tsvroot / fname for fname in os.listdir(tsvroot)]
 
 
-def load_tsv(tsvfile: Path) -> pa.dataset.Dataset:
+def load_tsv(tsvfile: Path) -> Optional[ds.Dataset]:
+    print()
     intcols = [f"i{i}" for i in range(1, 14)]
     strcols = [f"s{i}" for i in range(1, 27)]
     colnames = ["label"] + intcols + strcols
@@ -43,53 +66,105 @@ def load_tsv(tsvfile: Path) -> pa.dataset.Dataset:
     strschema = [(colname, pa.string()) for colname in strcols]
     schema = pa.schema(labelschema + intschema + strschema)
 
-    info_print(f"Loading {tsvfile} into dataset.")
-    format = pa.dataset.CsvFileFormat(
-        parse_options=pa.csv.ParseOptions(delimiter="\t"),
-        read_options=pa.csv.ReadOptions(column_names=colnames),
+    print(f"Loading {tsvfile} into dataset.")
+    format = ds.CsvFileFormat(
+        parse_options=pcsv.ParseOptions(delimiter="\t"),
+        read_options=pcsv.ReadOptions(column_names=colnames),
     )
 
-    return pa.dataset.dataset(source=tsvfile, schema=schema, format=format)
+    dataset = ds.dataset(source=tsvfile, schema=schema, format=format)
+
+    # Check that this is a TSV file in the expected format
+    try:
+        dataset.head(5)
+        return dataset
+    except ArrowInvalid:
+        warning_print(f"{tsvfile} is not a TSV file. Skipping.")
+        return None
 
 
-def write_parquet(ds: pa.dataset.Dataset, filename: str) -> Path:
-    dst = DATAROOT / "parquet" / filename
-    wo = pa.dataset.ParquetFileFormat().make_write_options(compression="gzip")
-    info_print(f"Writing dataset to {filename}.")
-    ds.write_dataset(ds, dst, format="parquet", file_options=wo)
-    return dst
+def write_parquet(dataroot: Path, dataset: ds.Dataset, filename: str) -> list[Path]:
+    print()
+    pqroot = dataroot / "parquet"
+    os.makedirs(pqroot, exist_ok=True)
+    wo = ds.ParquetFileFormat().make_write_options(compression="gzip")
+    print_now(f"Writing dataset as parquet to {pqroot}...", end="")
+    ds.write_dataset(
+        dataset,
+        pqroot,
+        format="parquet",
+        file_options=wo,
+        basename_template=f"{filename}_" + "{i}.parquet",
+        existing_data_behavior="overwrite_or_ignore",
+    )
+    print_now("Complete.")
+    return [Path(f) for f in glob(str(pqroot / f"{filename}_*.parquet"))]
 
 
 def upload_to_s3(src: Path, dst_s3url: str) -> None:
-    s3 = boto3.client("s3")
+    print()
+    total_bytes = src.stat().st_size
+
     s3url = urlparse(dst_s3url)
     bucket = s3url.netloc
-    prefix = Path(s3url.path)
-    objname = prefix / src.name
+    prefix = Path(s3url.path).relative_to("/")
+    key = prefix / src.name
+    print(f"Uploading {src} to s3://{bucket}/{key}")
+    progress_bar = tqdm(total=total_bytes, unit="iB", unit_scale=True)
     try:
-        info_print(f"Uploading {src} to {dst_s3url}.")
-        resp = s3.upload_file(src, bucket, objname)
-    except ClientError as e:
+        s3 = boto3.client("s3")
+        s3.upload_file(
+            str(src),
+            bucket,
+            str(key),
+            Callback=lambda chunk: progress_bar.update(chunk),
+        )
+    except ClientError as err:
         danger_print(f"Unable to upload {src} to {dst_s3url}!")
-        danger_print(e)
-        print(resp)
+        print(err)
 
 
 @click.command()
-@click.argument("download_url")
-@click.argument("upload_s3_url")
-def main(download_url, upload_s3_url):
+@click.option("--dataroot", type=click.Path(), default=".", help="Local data root.")
+@click.option(
+    "--s3url",
+    required=True,
+    help="The destination S3 URL s3://<bucket>/<prefix>/<filename>.<ext>.",
+)
+@click.option(
+    "--download-url",
+    help="The source url to download the tsv file from.",
+)
+def main(dataroot: str, s3url: str, download_url: Optional[str]) -> None:
     """
+    \b
     Tool to convert criteo CSV files to gzipped parquet files and upload them to S3.
       * Download csv file to local disk
       * Read csv file from disk to pyarrow dataset
       * Write pyarrow dataset as a parquet file to disk
       * Upload parquet file to S3
+    \b
+    Example:
+    prepare.py
+    --dataroot='/home/admin/mldata/criteo/kaggle'
+    --s3url='s3://avilabs-mldata-us-west-2/temp'
+    --download-url='https://go.criteo.net/criteo-research-kaggle-display-advertising-challenge-dataset.tar.gz'
     """
-    tsvpath: Path = download(download_url)
-    ds: pa.dataset.Dataset = load_tsv(tsvpath)
-    pqpath: Path = write_parquet(ds, tsvpath.name)
-    upload_to_s3(pqpath, upload_s3_url)
+    dataroot: Path = Path(dataroot)
+    if download_url:
+        downloaded_files: list[Path] = download(dataroot, download_url)
+    else:
+        tsvroot = dataroot / "tsv"
+        downloaded_files = [tsvroot / fname for fname in os.listdir(tsvroot)]
+    for downloaded_file in downloaded_files:
+        dataset: Optional[ds.Dataset] = load_tsv(downloaded_file)
+        files_to_upload: list[Path] = (
+            write_parquet(dataroot, dataset, downloaded_file.stem)
+            if dataset
+            else [downloaded_file]
+        )
+        for file_to_upload in files_to_upload:
+            upload_to_s3(file_to_upload, s3url)
 
 
 if __name__ == "__main__":
